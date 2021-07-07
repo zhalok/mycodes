7df89bd894821efc038f12a4c4cff0589dc5cae2
Zhalok <zhalokrahman007@gmail.com>
2021-07-07 18:46:10 +0600

    update

--- Run_code.js
+++ Run_code.js
@@ -2,12 +2,14 @@ const { exec } = require("child_process");
 const { stdin } = require("process");
 const filename = "F:\\cpp\\test";
 const data = "F:\\cpp\\input.txt";
-exec(`${filename}<${data}`, { prompt: "cmd.exe" }, (err, stdout, stderr) => {
-  // console.log("compiled");
-  console.log(stdout);
+exec(`g++ ${filename}`, (err, stdout, stderr) => {
+  console.log("compiled");
+  exec(`${filename}<${data}`, (err, stdout, stderr) => {
+    console.log("runned");
+    if (err) {
+      console.log("there was a problem");
+      return;
+    }
+    console.log(stdout);
+  });
 });
-
-// const fs = require("fs");
-// console.log(fs);
-// fs.writeFileSync("Hello.txt", "Hello guys\n");
-// fs.appendFileSync("Hello.txt", " hi");
