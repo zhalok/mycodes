b554b867f90ab6cea5fae1ff55426c2b18e7a2bc
zhalok <zhalok>
2022-08-08 11:38:30 +0600

    .

---
+++ dsp/fft.py
@@ -0,0 +1,33 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def fft(x):
+    n = len(x)
+    if len(x) == 1:
+        return x
+
+    w = np.exp(1j*2*np.pi/n)
+    even_list = []
+    odd_list = []
+    for i in range(math.floor(n/2)):
+        even_list.append(x[i*2])
+        odd_list.append(x[2*i+1])
+
+    _even_list, _odd_list = fft(even_list), fft(odd_list)
+    y = [0]*n
+    for i in range(math.floor(n/2)):
+        y[i] = _even_list[i] + np.power(w, i) * _odd_list[i]
+        y[i+math.floor(n/2)] = _even_list[i] - np.power(w, i) * odd_list[i]
+
+    return y
+
+
+signal = [1, 2, 3, 4, 5, 6, 7, 8]
+fft_response = fft(signal)
+
+plt.plot(signal, fft_response)
+plt.plot(signal, np.fft.fft(signal))
+plt.show()
+print(fft_response)
